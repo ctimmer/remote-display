0e33622416555a2e807f761dfd548cf7bdeca2af
Curt Timmerman <ctimmer@mtaonline.net>
2025-01-11 12:43:55 -0900

    demo-st7789 changes

--- demo-st7789/display_screen.py
+++ demo-st7789/display_screen.py
@@ -1,5 +1,8 @@
 #
 
+JSON_FILE_NAME = "display_screen.json"
+JSON_FILE_INDENT = 2 # 1-8 for pretty print / None for minimize
+
 DEMO_SCREEN = {
     "area_id": "demoscreen" ,
     "vpos": 0 ,
@@ -13,19 +16,19 @@ DEMO_SCREEN = {
     "areas" : [
             {
             "type" : "sysfont" ,
-            "vpos" : 0 ,
+            "vpos" : 2 ,
             "vlen" : 20 ,
-            "hpos" : 0 ,
-            "hlen" : 160 ,
+            "hpos" : 76 ,
+            "hlen" : 156 ,
             "paddingwidth" : 2 ,
-            "text" : "Hello World!" ,
-            "backgroundcolorname" : "YELLOW" ,
-            "textcolorname" : "RED"
+            "text" : " st7789 Demo" ,
+            "backgroundcolorname" : "WHITE" ,
+            "textcolorrgb" : (128, 0, 0)
             } ,
             {
             "type" : "sysfont" ,
             "area_id" : "text_output" ,
-            "vpos" : 40 ,
+            "vpos" : 28 ,
             "vlen" : 20 ,
             "hpos" : 10 ,
             "hlen" : 288 ,
@@ -35,30 +38,74 @@ DEMO_SCREEN = {
             "textcolorname" : "RED"
             } ,
             {
+            "type" : "container" ,
+            "area_id" : "scroll" ,
+            "vpos": 54 ,
+            "vlen": 106  ,
+            "hpos": 2 ,
+            "hlen": 304 ,
+            "paddingwidth" : 1 ,
+            "backgroundcolorname" : "BLACK" ,
+            "areas" : [
+                ]
+            } ,
+            {
             "type" : "7seg" ,
             "area_id" : "7seg_output" ,
             "digit_size" : "l" ,
             "bold" : True ,
-            "vpos" : 80 ,
+            "vpos" : 164 ,
             "vlen" : 62 ,
-            "hpos" : 60 ,
+            "hpos" : 54 ,
             "hlen" : 200 ,
             "paddingwidth" : 2 ,
-            "text" : "11:30" ,
+            "text" : "12:34" ,
             "backgroundcolorname" : "CYAN" ,
             "textcolorname" : "BLACK"
-            } ,
-            {
-            "type" : "sysfont" ,
-            "area_id" : "text_end" ,
-            "vpos" : 200 ,
-            "vlen" : 20 ,
-            "hpos" : 30 ,
-            "hlen" : 250 ,
-            "paddingwidth" : 2 ,
-            "text" : "" ,
-            "backgroundcolorname" : "YELLOW" ,
-            "textcolorname" : "RED"
-            } ,
+            }
         ]
     }
+SCROLL_VPOS = 0
+SCROLL_VPOS_INC = 21
+SCROLL_VLEN = 20
+SCROLL_HPOS = 0
+SCROLL_HLEN = 288
+SCROLL_LINE = {
+    "type" : "sysfont" ,
+    "area_id" : None ,
+    "vpos" : None ,
+    "vlen" : 20 ,
+    "hpos" : 0 ,
+    "hlen" : 302 ,
+    "paddingwidth" : 2 ,
+    "text" : "" ,
+    "backgroundcolorrgb" : (204, 255, 102) ,
+    "textcolorname" : "BLACK"
+    }
+vpos = SCROLL_VPOS
+SCROLL_AREA_IDS = ["line1", "line2", "line3", "line4", "line5"]
+scroll_text = []
+for area_id in SCROLL_AREA_IDS :
+    scroll_line = SCROLL_LINE.copy ()
+    scroll_line["area_id"] = area_id
+    scroll_line["vpos"] = vpos
+    DEMO_SCREEN["areas"][2]["areas"].append (scroll_line)
+    scroll_text.append (scroll_line["text"])
+    vpos += SCROLL_VPOS_INC
+
+if __name__ == "__main__" :
+    import json
+    print ("Building JSON display configuration:" ,
+           JSON_FILE_NAME)
+    json_dict = {
+        "DEMO_SCREEN" : DEMO_SCREEN
+    }
+    with open (JSON_FILE_NAME, "w") as json_fp :
+        json.dump (DEMO_SCREEN ,
+                    json_fp ,
+                    ensure_ascii = True ,
+                    check_circular = True ,
+                    indent = JSON_FILE_INDENT)
+
+## end __MAIN__ #
+
--- demo-st7789/rem_demo.py
+++ demo-st7789/rem_demo.py
@@ -24,6 +24,8 @@
 ################################################################################
 #
 
+import sys
+import os
 from machine import Pin, SPI
 import time
 
@@ -50,6 +52,49 @@ HEIGHT = const (320)
 WIDTH = const (240)
 ROTATION = const (3)    # landscape 1 or 3
 
+#-------------------------------------------------------------------------------
+## text_split - split text into string array at word boundries
+def text_split (text_in, max_len = 30) :
+    #print (text_in)
+    text_return = []
+    text_words = text_in.split ()
+    word_idx = 0
+    #print (text_words)
+    text_line = ""
+    while word_idx < len (text_words) :
+        #print (text_words [word_idx])
+        append_len = max_len
+        if len (text_words [word_idx]) > max_len :
+            if len (text_line) > 0 :
+                len_left = max_len - len (text_line)
+                if len_left >= 4 :
+                    append_len -= len_left
+                    len_left -= 1
+                    text_line += " " + (text_words [word_idx][0:len_left])
+                    text_words [word_idx] = text_words [word_idx][len_left:]
+                text_return.append (text_line)
+                text_line = ""
+            #text_return.append (text_words [word_idx][0:append_len])
+            #text_words [word_idx] = text_words [word_idx][append_len:]
+            continue
+        if len (text_line) + len (text_words [word_idx]) + 1 > max_len :
+            if len (text_line) > 0 :
+                text_return.append (text_line)
+                text_line = ""
+        if len (text_line) > 0 :
+            #text_line += " "
+            text_line += " "
+        text_line += text_words [word_idx]
+        word_idx += 1
+    if len (text_line) > 0 :
+        text_return.append (text_line)
+    #print (text_return)
+    return text_return
+
+#-------------------------------------------------------------------------------
+# main
+#-------------------------------------------------------------------------------
+
 if FREQ > 0 :
     machine.freq (FREQ)
 
@@ -73,7 +118,13 @@ display.add_area_type ("7seg", Remote7Segment)
 
 display.setup_config_dict (display_screen.DEMO_SCREEN)
 
-time.sleep (5)
+'''
+display.screen_clear ()
+display.show_area ()
+sys.exit ()
+'''
+
+time.sleep (2)
 display.update_area (area_id = "text_output" ,
                      text = "Now is the time")
 
@@ -89,8 +140,32 @@ time.sleep (2)
 display.update_area (area_id = "text_output" ,
                      text = "of their country")
 
-time.sleep (2)
-display.update_area (area_id = "text_end" ,
+## Scrolling text
+os_info = os.uname ()
+os_text = ["OS INFORMATION"]
+
+MAX_TEXT_LEN = 25
+os_text += text_split ("SYSNAME: " + os_info.sysname, max_len = MAX_TEXT_LEN)
+os_text += text_split ("RELEASE: " + os_info.release, max_len = MAX_TEXT_LEN)
+os_text += text_split ("VERSION: " + os_info.version, max_len = MAX_TEXT_LEN)
+os_text += text_split ("MACHINE: " + os_info.machine, max_len = MAX_TEXT_LEN)
+#print (os_text)
+
+scroll_area_ids = display_screen.SCROLL_AREA_IDS
+scroll_text = display_screen.scroll_text
+for os_text_idx in range (0, len (os_text)) :
+    scroll_idx = 1
+    for scroll_idx in range (scroll_idx, len (scroll_text)) :
+        scroll_text [scroll_idx - 1] = scroll_text [scroll_idx]
+    scroll_text [scroll_idx] = os_text [os_text_idx]
+    scroll_idx = 0
+    for area_id in scroll_area_ids :
+        display.update_area (area_id = area_id, text = scroll_text [scroll_idx])
+        scroll_idx += 1
+    time.sleep (2.0)
+
+#time.sleep (2)
+display.update_area (area_id = "text_output" ,
                      text = "End of Demonstration")
 
 print ("End of Demonstration")
